950a98cd45fe016a82cd9d106af4cd4740b1980b
Abenezer Kebede <73950771+abeni5049@users.noreply.github.com>
2022-01-05 21:10:02 +0300

    Create RecentCounter.py

---
+++ RecentCounter.py
@@ -0,0 +1,10 @@
+class RecentCounter:
+
+    def __init__(self):
+        self.requests = deque()
+
+    def ping(self, t: int) -> int:
+        self.requests.append(t)
+        while self.requests[0] < t - 3000:
+            self.requests.popleft()     
+        return len(self.requests)
