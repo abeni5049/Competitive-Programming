4217360185b29a8b519893562fef8d5c294f0c29
Abenezer Kebede <73950771+abeni5049@users.noreply.github.com>
2022-01-13 15:40:18 +0300

    Create validateStackSequences.py

---
+++ validateStackSequences.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def validateStackSequences(self, pushed, popped):
+        j = 0
+        stack = []
+        for x in pushed:
+            stack.append(x)
+            while stack and stack[-1] == popped[j]:
+                stack.pop()
+                j += 1
+        return j == len(popped)
