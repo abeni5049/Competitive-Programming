dcb0e451bf63cc178d2b2eb43452533b869810f5
Abenezer Kebede <73950771+abeni5049@users.noreply.github.com>
2022-01-20 13:18:41 +0300

    Update minNonZeroProduct.py

--- minNonZeroProduct.py
+++ minNonZeroProduct.py
@@ -1,5 +1,16 @@
 class Solution:
     def minNonZeroProduct(self, p: int) -> int:
+        
+        def pow(num,p,limit):
+            if p == 0:
+                return 1
+            if p == 1:
+                return num
+            res = pow(num,p//2,limit)**2
+            if p % 2:
+                res *= num
+            return res % limit
+        
         limit = 10**9+7
         maxNum = 2**p -1
         res = 1
