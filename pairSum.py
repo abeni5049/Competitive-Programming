0ede1fe952dc1604106e83e93bcf0556f14f843e
Abenezer Kebede <73950771+abeni5049@users.noreply.github.com>
2022-02-23 01:12:47 +0300

    Update pairSum.py

--- pairSum.py
+++ pairSum.py
@@ -5,14 +5,36 @@
 #         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        curr = head
-        arr = []
+        """
+        first find middle node using slow and fast pointers
+        reverse from middle to last 
+        ith element of original linked list is twin with ith node of reversed linkedlist
+        compare the sum of current twin with prev twin and store the max
+        return the max sum
+        """
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        prev = None
+        curr = slow
         while curr:
-            arr.append(curr.val)
-            curr = curr.next
-        
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+            
         res = -1
-        for i in range(len(arr)//2):
-            currSum = arr[i] + arr[len(arr)-1-i]
+        originalList = head
+        reversedList = prev
+        while originalList and reversedList:
+            currSum = originalList.val + reversedList.val
             res = max(res,currSum)
+            originalList = originalList.next
+            reversedList = reversedList.next
         return res
+        
+        
+        
