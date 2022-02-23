0369154ae109f1bcdd796d20477f1d94950ab29a
Abenezer Kebede <73950771+abeni5049@users.noreply.github.com>
2022-02-23 12:39:17 +0300

    Create has_cycle.py

---
+++ has_cycle.py
@@ -0,0 +1,9 @@
+def has_cycle(head):
+    slow = head
+    fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            return 1
+    return 0
