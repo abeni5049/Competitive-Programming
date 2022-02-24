4b04f4bf2985bf4ff886ba942981a687fc5b931c
Abenezer Kebede <73950771+abeni5049@users.noreply.github.com>
2022-02-24 15:47:49 +0300

    Update middleNode.py

--- middleNode.py
+++ middleNode.py
@@ -5,16 +5,8 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        count = 0
-        curr = head
-        while curr != None:
-            count += 1
-            curr = curr.next
-            
-        mid = count//2
-        curr = head
-        count = 0
-        while count < mid:
-            curr = curr.next
-            count += 1
-        return curr
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
