a0aaa4504aa3e96ee5ecee84b180fff92e761e78
Abenezer Kebede <73950771+abeni5049@users.noreply.github.com>
2022-03-01 18:03:32 +0300

    Update rotate.py

--- rotate.py
+++ rotate.py
@@ -4,6 +4,16 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """   
         k = k % len(nums)
-        k = len(nums) - k
-        nums[:] = nums[k:] + nums[:k]
+        nums[:] = self.reverse(nums)
+        nums[:k] = self.reverse(nums[:k])
+        nums[k:] = self.reverse(nums[k:])
+        
+    def reverse(self,arr):
+        l = 0
+        r = len(arr) - 1
+        while l < r:
+            arr[l],arr[r] = arr[r],arr[l]
+            l += 1
+            r -= 1
+        return arr
         
