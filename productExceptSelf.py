11468b417d1509fd89f95fa9a4e6abb7e927114d
Abenezer Kebede <73950771+abeni5049@users.noreply.github.com>
2022-03-04 01:58:25 +0300

    Update productExceptSelf.py

--- productExceptSelf.py
+++ productExceptSelf.py
@@ -1,14 +1,15 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         
-        revPrefix = [nums[len(nums)-1]]
-        for i in range(len(nums)-2,-1,-1):
-            revPrefix.append(revPrefix[-1]*nums[i])
+        res = []
+        prefix = 1
+        for num in nums:
+            res.append(prefix)
+            prefix *= num
             
-        prefix = [nums[0]]
-        res = [revPrefix[-2]]
-        for j in range(1,len(nums)-1):
-            prefix.append(prefix[-1]*nums[j])
-            res.append(prefix[j-1]*revPrefix[-j-2])    
-        res.append(prefix[len(nums)-2])
+        postfix = 1
+        for i in range(len(nums)-1,-1,-1):
+            res[i] *= postfix
+            postfix *= nums[i]
+            
         return res
