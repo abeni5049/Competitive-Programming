105ea926a36824da16ec085ec0bf6cb976ffcf2f
Abenezer Kebede <73950771+abeni5049@users.noreply.github.com>
2022-08-12 14:19:49 +0300

    Create diameterOfBinaryTree.py

---
+++ diameterOfBinaryTree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        res = 0
+        def dfs(root):
+            nonlocal res
+            if not root: return 0
+            l = dfs(root.left)
+            r = dfs(root.right)
+            res = max(res,l+r)
+            return 1 + max(l,r)
+
+        dfs(root)
+        return res
