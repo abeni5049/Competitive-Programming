fcf8f56f6f97002db456f7db68e342bd59534c3a
Abenezer Kebede <73950771+abeni5049@users.noreply.github.com>
2022-10-04 14:56:17 +0300

    Update sumSubarrayMins.py

--- sumSubarrayMins.py
+++ sumSubarrayMins.py
@@ -7,15 +7,12 @@ class Solution:
             while mstack and arr[mstack[-1]] >= num:
                 ind = mstack.pop()
                 left = mstack[-1] if mstack else -1
-                res += arr[ind] * (i-ind) * (ind-left if ind != left else 1)
+                res += arr[ind] * (i-ind) * (ind-left)
             mstack.append(i)
         
-        while len(mstack)>1:
+        while mstack:
             ind = mstack.pop()
-            res += arr[ind] * (len(arr)-ind) * (ind-mstack[-1])
+            left = mstack[-1] if mstack else -1
+            res += arr[ind] * (len(arr)-ind) * (ind-left)
             
-        if mstack:
-            ind = mstack.pop()
-            res += arr[ind] * (ind+1) * (len(arr)-ind)
-        
         return res%mod
