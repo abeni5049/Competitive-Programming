8eaf0adbd572473113574640caf0e7fce84ae7a5
Abenezer Kebede <73950771+abeni5049@users.noreply.github.com>
2022-10-23 16:08:26 +0300

    Create maxProfit4.py

---
+++ maxProfit4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        @cache
+        def dfs(i,state,count):
+            if i == len(prices) or count == k: return 0
+            if state == 0:
+                buy = dfs(i+1,1,count)-prices[i]
+                dont_buy = dfs(i+1,state,count)
+                return max(buy,dont_buy)
+            else:
+                sell = dfs(i+1,0,count+1)+prices[i]
+                dont_sell = dfs(i+1,state,count)
+                return max(sell,dont_sell)
+        return dfs(0,0,0)
