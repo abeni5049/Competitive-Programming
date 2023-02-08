44705a811c456e123f4daa04f7e5c2f35b0873b9
Abenezer Kebede <73950771+abeni5049@users.noreply.github.com>
2023-02-08 21:22:16 +0300

    Create thirdMax.py

---
+++ thirdMax.py
@@ -0,0 +1,7 @@
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums = list(set(nums))
+        nums.sort(reverse=True)
+        if len(nums) < 3:
+            return nums[0]
+        return nums[2]
